676926eb2c2d5efd912da027f86d162fd410c113
noelfoka <nomgnenoel@gmail.com>
2023-06-19 11:25:54 +0100

    comment

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
 // eslint-disable linebreak-style
-const menuButton = document.querySelector("#menu-icon");
+/*const menuButton = document.querySelector("#menu-icon");
 const exitMenuButton = document.querySelector("#exit-menu-icon");
 const menuLinks = document.querySelectorAll(".mobile-menu-link");
 const menuBtns = [menuButton, exitMenuButton];
 const menuContainer = document.querySelector(".mobile-menu-container");
 const wholePage = document.querySelector("html");
+*/
 
-menuBtns.map((menuBtn) =>
+/*menuBtns.map((menuBtn) =>
   menuBtn.addEventListener("click", () => {
     menuContainer.classList.toggle("mobile-menu-toggler");
     wholePage.classList.toggle("overflow-none");
   })
 );
+*/
 
 menuLinks.forEach((menuLink) => {
   menuLink.addEventListener("click", () => {
