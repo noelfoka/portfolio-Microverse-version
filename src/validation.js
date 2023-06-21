7a08e02420b7594fc54526e51fd23fb33f4a765f
noelfoka <nomgnenoel@gmail.com>
2023-06-21 07:22:39 +0100

    update validation

--- src/validation.js
+++ src/validation.js
@@ -1,6 +1,6 @@
-/*export const email = document.querySelector('.contact__email');
-export const form = document.querySelector('#form');
-export const error = document.querySelector('.error');
+export const email = document.querySelector(".contact__email");
+export const form = document.querySelector("#form");
+export const error = document.querySelector(".error");
 
 export function isItUppercase(value) {
   if (value.match(/^[a-z@.0-9-_]*$/)) {
@@ -8,4 +8,3 @@ export function isItUppercase(value) {
   }
   return false;
 }
-*/
