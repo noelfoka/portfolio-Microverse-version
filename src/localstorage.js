c4e577360a159bc680f693a374d06d46c2fa85c2
noelfoka <nomgnenoel@gmail.com>
2023-06-21 13:36:37 +0100

    update local storage

--- src/localstorage.js
+++ src/localstorage.js
@@ -1,29 +1,28 @@
-export const name = document.querySelector(".contact__name");
+export const name = document.querySelector('.contact__name');
 
 export function storageAvailable(type) {
   let storage;
   try {
     storage = window[type];
-    const x = "__storage_test__";
+    const x = '__storage_test__';
     storage.setItem(x, x);
     storage.removeItem(x);
     return true;
   } catch (e) {
     return (
-      e instanceof DOMException &&
+      e instanceof DOMException
       // everything except firefox
-      (e.code === 22 ||
+      && (e.code === 22
         // firefox
-        e.code === 1014 ||
+        || e.code === 1014
         // test name field too, because code might not be present
         // everything except firefox
-        e.name === "QuotaExceededError" ||
+        || e.name === 'QuotaExceededError'
         // firefox
-        e.name === "NS_ERROR_DOM_QUOTA_REACHED") &&
+        || e.name === 'NS_ERROR_DOM_QUOTA_REACHED')
       // acknowledge QuotaExceededError only if there's something already stored
-      storage &&
-      storage.length !== 0
+      && storage
+      && storage.length !== 0
     );
   }
 }
-*/
